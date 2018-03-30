92e1f8229736c4364c1a7360050960666badc3df
MAX <lupascu.maxim@gmail.com>
2018-03-30 19:48:31 +0300

    practise 4

---
+++ leasson4/ShopingCard/src/practice_04_1/ShopingCard.java
@@ -0,0 +1,11 @@
+package practice_04_1;
+
+public class ShopingCard {
+    public static void main(String[] args){
+        String custName = "Mary Smith";
+        String itemDesc = "Shirt";
+        String message;
+        message =custName + " wants to purchase a " +itemDesc;
+        System.out.println(message);
+    }
+}
