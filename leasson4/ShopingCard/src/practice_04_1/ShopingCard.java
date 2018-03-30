3257e46fac77b3998dcd7ac9ee2046d28d9a2a80
MAX <lupascu.maxim@gmail.com>
2018-03-30 20:18:14 +0300

    practise 4 with add 2 exemple

--- leasson4/ShopingCard/src/practice_04_1/ShopingCard.java
+++ leasson4/ShopingCard/src/practice_04_1/ShopingCard.java
@@ -5,7 +5,13 @@ public class ShopingCard {
         String custName = "Mary Smith";
         String itemDesc = "Shirt";
         String message;
-        message =custName + " wants to purchase a " +itemDesc;
+
+        double price=100,tax = 2.5;
+        double total;
+        int quantity = 3;
+        total =price*quantity*tax;
+        message =custName + " wants to purchase " + quantity + " "+itemDesc;
         System.out.println(message);
+        System.out.println("Total cost with tax is:"+total);
     }
 }
