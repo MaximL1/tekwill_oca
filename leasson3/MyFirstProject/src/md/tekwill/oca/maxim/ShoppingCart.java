6d018c657484388ffbaeffc2259f3916c1d745c2
MAX <lupascu.maxim@gmail.com>
2018-03-30 14:48:43 +0300

    add file

--- leasson3/MyFirstProject/src/md/tekwill/oca/maxim/ShoppingCart.java
+++ leasson3/MyFirstProject/src/md/tekwill/oca/maxim/ShoppingCart.java
@@ -2,6 +2,6 @@ package md.tekwill.oca.maxim;
 
 public class ShoppingCart {
     public static void main(String[] arg) {
-        System.out.println("Hello Java!");
+        System.out.println("Hello Java!!!");
     }
 }
